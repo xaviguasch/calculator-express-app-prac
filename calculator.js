00a8bcc62ae13fc9b373c79b7ccf242892cff773
xaviguasch <xaviguasch@gmail.com>
2019-02-05 17:40:44 +0100

    setting up calculator page

--- calculator.js
+++ calculator.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-    res.send('this is the calculator app')
+    res.sendFile(`${__dirname}/index.html`);
 })
 
 
