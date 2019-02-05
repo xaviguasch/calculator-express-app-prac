eb04767826905c18fac5d58c6f7d8d29d005647b
xaviguasch <xaviguasch@gmail.com>
2019-02-05 18:21:49 +0100

    setting up post routes and bodyParser

--- calculator.js
+++ calculator.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
 app.get('/', function (req, res) {
     res.sendFile(`${__dirname}/index.html`);
+})
+
+app.post('/', function (req, res) {
+    res.send('thanks for posting that')
 })
 
 
